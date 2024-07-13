128c17dcdacfccb4b41e48a75287b4107060709a
Pileks <jure@amber-it.co>
2024-07-13 15:09:20 +0200

    additional buying tests

--- tests/whitelist_token_sale.ts
+++ tests/whitelist_token_sale.ts
@@ -35,14 +35,14 @@ describe("Whitelist Token Sale - story", () => {
 
   const SALE_NAME = "a token sale";
   const SALE_PRICE_PER_TOKEN_LAMPORTS = new BN(LAMPORTS_PER_SOL / 100); // 1 token = 0.01 SOL
-  const SALE_MAX_BUYERS = new BN(10);
+  const SALE_MAX_BUYERS = new BN(2);
   const SALE_MAX_TOKENS_PER_BUYER = new BN(1000);
 
   // Actors in our tests
   const MINT_KEYPAIR = Keypair.generate();
   const OWNER_KEYPAIR = Keypair.generate();
-  const BUYER_KEYPAIR_1 = Keypair.generate();
-  const BUYER_KEYPAIR_2 = Keypair.generate();
+  const BUYER_KEYPAIR = Keypair.generate();
+  const NON_BUYER_KEYPAIR = Keypair.generate();
 
   // We will set these in the before() call
   let ownerAta: Account;
@@ -93,8 +93,8 @@ describe("Whitelist Token Sale - story", () => {
 
   before("airdrop SOL into wallets and create a token mint", async () => {
     await airdropSol(OWNER_KEYPAIR.publicKey, 100);
-    await airdropSol(BUYER_KEYPAIR_1.publicKey, 100);
-    await airdropSol(BUYER_KEYPAIR_2.publicKey, 100);
+    await airdropSol(BUYER_KEYPAIR.publicKey, 100);
+    await airdropSol(NON_BUYER_KEYPAIR.publicKey, 100);
 
     await createMint(
       provider.connection,
@@ -125,16 +125,16 @@ describe("Whitelist Token Sale - story", () => {
 
     buyerAta1 = await getOrCreateAssociatedTokenAccount(
       provider.connection,
-      BUYER_KEYPAIR_1,
+      BUYER_KEYPAIR,
       MINT_KEYPAIR.publicKey,
-      BUYER_KEYPAIR_1.publicKey
+      BUYER_KEYPAIR.publicKey
     );
 
     buyerAta2 = await getOrCreateAssociatedTokenAccount(
       provider.connection,
-      BUYER_KEYPAIR_2,
+      NON_BUYER_KEYPAIR,
       MINT_KEYPAIR.publicKey,
-      BUYER_KEYPAIR_2.publicKey
+      NON_BUYER_KEYPAIR.publicKey
     );
   });
 
@@ -208,27 +208,27 @@ describe("Whitelist Token Sale - story", () => {
     await program.methods
       .registerForWhitelist(SALE_NAME)
       .accounts({
-        signer: BUYER_KEYPAIR_1.publicKey,
+        signer: BUYER_KEYPAIR.publicKey,
       })
-      .signers([BUYER_KEYPAIR_1])
+      .signers([BUYER_KEYPAIR])
       .rpc();
 
     const allowanceAddress = getAllowanceAddress(
       SALE_NAME,
-      BUYER_KEYPAIR_1.publicKey
+      BUYER_KEYPAIR.publicKey
     );
     const allowance = await program.account.allowance.fetch(allowanceAddress);
 
     assert.equal(allowance.tokensBought.cmp(new BN(0)), 0);
   });
 
-  it("should error when an already registered buyer tries to register on whitelist", async () => {
+  it("should disallow an already registered buyer tries to register on whitelist", async () => {
     await program.methods
       .registerForWhitelist(SALE_NAME)
       .accounts({
-        signer: BUYER_KEYPAIR_1.publicKey,
+        signer: BUYER_KEYPAIR.publicKey,
       })
-      .signers([BUYER_KEYPAIR_1])
+      .signers([BUYER_KEYPAIR])
       .rpc()
       .then(
         () => {
@@ -240,7 +240,7 @@ describe("Whitelist Token Sale - story", () => {
           assert.ok(
             e.logs.some(
               (log) =>
-                log.includes("account Address") &&
+                log.includes("Allocate: account Address") &&
                 log.includes("already in use")
             )
           );
@@ -252,20 +252,16 @@ describe("Whitelist Token Sale - story", () => {
     await program.methods
       .updateSaleState(SALE_NAME, false, true)
       .accounts({
-        signer: BUYER_KEYPAIR_1.publicKey,
+        signer: BUYER_KEYPAIR.publicKey,
       })
-      .signers([BUYER_KEYPAIR_1])
+      .signers([BUYER_KEYPAIR])
       .rpc()
       .then(
         () => {
           assert.fail("Non-owner should not be able to change sale state!");
         },
         (e: SendTransactionError) => {
-          assert.ok(
-            e.logs.some((log) =>
-              log.includes("Only the sale owner can perform this action")
-            )
-          );
+          assert.ok(e.logs.some((log) => log.includes("OnlyOwner")));
         }
       );
 
@@ -284,21 +280,19 @@ describe("Whitelist Token Sale - story", () => {
     await program.methods
       .buyTokens(SALE_NAME, buyAmount)
       .accounts({
-        signer: BUYER_KEYPAIR_1.publicKey,
+        signer: BUYER_KEYPAIR.publicKey,
         tokenMint: MINT_KEYPAIR.publicKey,
       })
-      .signers([BUYER_KEYPAIR_1])
+      .signers([BUYER_KEYPAIR])
       .rpc()
       .then(
         () => {
-          assert.fail("Whitelisted buyer should not be able to buy before token sale is open!");
+          assert.fail(
+            "Whitelisted buyer should not be able to buy before token sale is open!"
+          );
         },
         (e: SendTransactionError) => {
-          assert.ok(
-            e.logs.some((log) =>
-              log.includes("Whiteliste sale is closed")
-            )
-          );
+          assert.ok(e.logs.some((log) => log.includes("SaleClosed")));
         }
       );
   });
@@ -322,20 +316,20 @@ describe("Whitelist Token Sale - story", () => {
   });
 
   it("should allow whitelisted buyer to purchase tokens once sale is open", async () => {
-    const buyAmount = new BN(10);
+    const buyAmount = new BN(999);
 
     await program.methods
       .buyTokens(SALE_NAME, buyAmount)
       .accounts({
-        signer: BUYER_KEYPAIR_1.publicKey,
+        signer: BUYER_KEYPAIR.publicKey,
         tokenMint: MINT_KEYPAIR.publicKey,
       })
-      .signers([BUYER_KEYPAIR_1])
+      .signers([BUYER_KEYPAIR])
       .rpc();
 
     const signerAtaAddress = await getAssociatedTokenAddress(
       MINT_KEYPAIR.publicKey,
-      BUYER_KEYPAIR_1.publicKey
+      BUYER_KEYPAIR.publicKey
     );
     const signerAta = await getAccount(provider.connection, signerAtaAddress);
 
@@ -347,13 +341,90 @@ describe("Whitelist Token Sale - story", () => {
     );
   });
 
+  it("should disallow whitelisted buyer to purchase more tokens than allocated", async () => {
+    // Buy limit is 1000. We already bought 999 tokens, so this should fail
+    const buyAmount = new BN(2);
+
+    await program.methods
+      .buyTokens(SALE_NAME, buyAmount)
+      .accounts({
+        signer: BUYER_KEYPAIR.publicKey,
+        tokenMint: MINT_KEYPAIR.publicKey,
+      })
+      .signers([BUYER_KEYPAIR])
+      .rpc()
+      .then(
+        () => {
+          assert.fail(
+            "Whitelisted buyer should not be able to buy more than the allocated amount!"
+          );
+        },
+        (e: SendTransactionError) => {
+          assert.ok(e.logs.some((log) => log.includes("AllowanceExceeded")));
+        }
+      );
+  });
+
+  it("should allow the same purchaser to purchase more than once, up to the total allowance", async () => {
+    // Let's buy the remaining 1 token...
+    const remainingBuyAmount = new BN(1);
+
+    await program.methods
+      .buyTokens(SALE_NAME, remainingBuyAmount)
+      .accounts({
+        signer: BUYER_KEYPAIR.publicKey,
+        tokenMint: MINT_KEYPAIR.publicKey,
+      })
+      .signers([BUYER_KEYPAIR])
+      .rpc();
+
+    const signerAtaAddress = await getAssociatedTokenAddress(
+      MINT_KEYPAIR.publicKey,
+      BUYER_KEYPAIR.publicKey
+    );
+    const signerAta = await getAccount(provider.connection, signerAtaAddress);
+    const expectedAmount = new BN(1000);
+
+    assert.isTrue(
+      new BN(signerAta.amount.toString()).eq(
+        expectedAmount.mul(new BN(Math.pow(10, DECIMALS)))
+      ),
+      `Singer's ATA amount is ${signerAta.amount}, while it should be ${expectedAmount}`
+    );
+  });
+
+  it("should disallow non-whitelisted buyer to purchase tokens", async () => {
+    const buyAmount = new BN(10);
+
+    await program.methods
+      .buyTokens(SALE_NAME, buyAmount)
+      .accounts({
+        signer: NON_BUYER_KEYPAIR.publicKey,
+        tokenMint: MINT_KEYPAIR.publicKey,
+      })
+      .signers([NON_BUYER_KEYPAIR])
+      .rpc()
+      .then(
+        () => {
+          assert.fail(
+            "Non-whitelisted buyer should not be able to purchase tokens!"
+          );
+        },
+        (e: SendTransactionError) => {
+          assert.ok(
+            e.logs.some((log) => log.includes("AccountNotInitialized"))
+          );
+        }
+      );
+  });
+
   it("should disallow a buyer to register on whitelist while registration is closed", async () => {
     await program.methods
       .registerForWhitelist(SALE_NAME)
       .accounts({
-        signer: BUYER_KEYPAIR_2.publicKey,
+        signer: NON_BUYER_KEYPAIR.publicKey,
       })
-      .signers([BUYER_KEYPAIR_2])
+      .signers([NON_BUYER_KEYPAIR])
       .rpc()
       .then(
         () => {
